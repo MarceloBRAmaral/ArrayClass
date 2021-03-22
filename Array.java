91b70c4160a0136dbc76a39a027ebf58ff13fd15
MarceloBRAmaral <marceloamaral.amaral@gmail.com>
2021-03-21 22:27:36 -0400

    first commit

---
+++ Array.java
@@ -0,0 +1,67 @@
+//import java.util.Arrays;
+
+public class Array {
+
+    private int[] items;//to be used as the main array
+    private int[] itemsCopy;//to be used as a copy of the main array when needed
+    private int index;
+    int indexOfArray;
+
+    //constructor to build new arrays
+    public Array(int length){
+        items = new int[length];
+    }
+
+    //method to print all array elements one per line
+    public void print(){
+        for (int i=0; i<items.length; i++){
+        System.out.println(items[i]);
+        }
+    }
+
+    //insert int values in the array starting from the index 0
+    //when the initial length is reached, the array length increases by one
+    public void insert(int item){
+        if (index<items.length){
+            items[index] = item;
+        }
+        else{
+            itemsCopy = items;
+            items = new int[itemsCopy.length+1];
+            for (int j=0; j<itemsCopy.length; j++){
+                items[j] = itemsCopy[j];
+            }
+            items[itemsCopy.length] = item;
+        }
+        index++;
+    }
+
+    //removes the array value at index position
+    public void removeAt(int index){
+        itemsCopy = items;
+        items = new int[itemsCopy.length-1];
+        for (int j=0; j<items.length; j++){
+            if (j<index) {
+                items[j] = itemsCopy[j];
+            }
+            else {
+                items[j] = itemsCopy[j + 1];
+            }
+        }
+    }
+
+    //searches for the first value in the array and gives its index
+    //if the value is not found, it gives a -1
+    public int indexOf(int value){
+
+        indexOfArray = -1;
+        for (int i=0; i<items.length; i++){
+
+            if(items[i]==value){
+                indexOfArray = i;
+                break;
+            }
+        }
+        return indexOfArray;
+    }
+}
---
+++ TestArray.java
@@ -0,0 +1,27 @@
+public class TestArray {
+
+    public static void main(String[] args) {
+
+        Array num = new Array(3);//builds the array with the given length
+
+        System.out.println("Just testing the methods present in Array class.");
+
+        //insert method inserts int values in the array
+        //if the array is full, it increases the array size and inserts the new value at the end of the array
+        num.insert(1);
+        num.insert(2);
+        num.insert(3);
+        num.insert(4);
+        num.insert(5);
+
+        //remove method removes the value at given index
+        //the array sizes is decreased by one each time the method is called
+        num.removeAt(3);
+
+        //print method prints each array value in new line
+        num.print();
+
+        //indexOf method prints the array value of a given method
+        System.out.println(num.indexOf(3));
+    }
+}
